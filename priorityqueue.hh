32e830e7c466a27c05ae4b7de7a5bf9ff0322345
mszkudla <259248@student.pwr.edu.pl>
2022-03-29 20:15:07 +0200

    dodanie kilku struktur i klas

--- priorityqueue.hh
+++ priorityqueue.hh
@@ -17,3 +17,68 @@ struct data2
     std::string info;
     bool datatype = false;
 };
+
+template <typename type>
+class queue
+{
+private:
+    type front *;
+    type back *;
+    int size;
+
+public:
+    queue()
+    {
+        front = back = NULL;
+        size = 0;
+    };
+    bool checkifempty();
+    int value();
+    type least() : type DELLleast();
+    void add(int priority, std::string info)
+};
+
+template <typename type>
+bool queue<type>::checkifempty()
+{
+    return !front;
+}
+
+template <typename type>
+int queue<type>::value()
+{
+    return size;
+}
+
+template <typename type>
+type queue<type>::least()
+{
+    if (queue<type>::checkifempty())
+    {
+        throw std::logic_error("Lista jest pusta!");
+    }
+    type *ptr1, *ptr2;
+    ptr1 = ptr2 = front;
+    if (ptr1 != NULL)
+    {
+        do
+        {
+            if (ptr1->datatype)
+            {
+                ptr2 = ptr2->next;
+                if (ptr2->priority < ptr1->priority)
+                {
+                    ptr1 = ptr2;
+                }
+                else
+                {
+                    ptr2 = ptr2->next;
+                    if (ptr2->priority2 < ptr1->priority2)
+                        ptr1 = ptr2;
+                }
+            }
+
+        } while (ptr2 != back);
+    }
+    return *ptr1;
+}
